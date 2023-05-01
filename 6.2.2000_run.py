ea26842e363a92dc37353217799e155d74a99157
RubenYoo <rubenchiche0317@gmail.com>
2023-05-01 21:43:23 +0300

    fixing issues

--- 6.2.2000_run.py
+++ 6.2.2000_run.py
@@ -1,14 +1,18 @@
 import time
 
 
-def timer(f, *args, **kwargs):
-    t1 = time.time()
-    f(*args, **kwargs)
-    return time.time() - t1
+def timer(func, *args, **kwargs):
+    start_time = time.time()
+    func(*args, **kwargs)
+    return time.time() - start_time
 
 
-print(timer(print, "Hello"))
-print(timer(zip, [1, 2, 3], [4, 5, 6]))
-print(timer("Hi {name}".format, name="Bug"))
+def main():
+    print(timer(print, "Hello"))
+    print(timer(zip, [1, 2, 3], [4, 5, 6]))
+    print(timer("Hi {name}".format, name="Bug"))
+    print(timer(pow, 5, 5689179))
 
-print(timer(pow, 5, 5689179))
+
+if __name__ == "__main__":
+    main()
